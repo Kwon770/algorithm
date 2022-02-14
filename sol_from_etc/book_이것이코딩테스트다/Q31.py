fd412631940cceecf220ce1ee2cadcd5b7c149ea
Kwon770 <sckwon770@gmail.com>
2022-02-14 11:26:25 +0900

    book: Q31

---
+++ sol_from_etc/book_이것이코딩테스트다/Q31.py
@@ -0,0 +1,48 @@
+# DP - Q31 금광 (P375)
+
+# DP 전형적인 유형 : 캐싱이 없으면 고려해야할 경우의 수가 너무 많은 문제
+# 캐싱을 통해 O(N)을 만들려하지말고, "현 단계의 경우의 수가 다음 단계의 경우의 수에 영향을 미치지 않을 저도로만 만들면 됨"
+# 즉, 매번 현 단계에서 고려해야하는 모든 경우의 수를 확인하여 최적값을 캐싱하면 된다.
+
+# 현 단계가 다음 단계에 영향을 미치는 O(N^M)
+#       1 <
+# 1 <
+#       2 <
+#
+#       1 <
+# 2 <
+#       2 <
+
+import sys
+input = sys.stdin.readline
+
+drs = [-1, 0, 1]
+
+def solution():
+    for TC in range(int(input())):
+        n, m = map(int, input().split())
+        arr = list(map(int, input().split()))
+
+        memo = []
+        idx = 0
+        for r in range(n):
+            memo.append(arr[idx:idx+m])
+            idx += m
+
+        for c in range(1, m):
+            for r in range(n):
+                maxPr = -1
+                for dr in drs:
+                    pr = r + dr
+                    if pr == -1 or pr == n:
+                        continue
+
+                    maxPr = max(maxPr, memo[pr][c-1])
+                memo[r][c] += maxPr
+
+        result = -1
+        for r in range(n):
+            result = max(result, memo[r][-1])
+        print(result)
+
+print(solution())
