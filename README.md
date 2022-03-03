9d1ae73845899cb25b0cd567ccc1df6f78a07fdb
Kwon770 <sckwon770@gmail.com>
2022-03-03 19:00:15 +0900

    greedy: boj8980.py

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 |DP 전형적인 유형 : 캐싱이 없으면 고려해야할 경우의 수가 너무 많은 문제|[금광](./sol_from_etc/book_이것이코딩테스트다/Q31.py)|
 |원소가 두 개인 튜플 탐색|[뱀](./sol_from_BOJ/simulation/boj3190.py)|
 |배열을 90도 회전시키기|[자물쇠와 열쇠](./sol_from_Programmers/kakao_blind/pr60059.py)|
+|DP 문제처럼 보여도 그리디로 풀 수 있는 문제는?|[택배](./sol_from_BOJ/greedy/boj8980.py)|
---
+++ solution.py
@@ -0,0 +1,61 @@
+from collections import deque
+import heapq
+
+INF = int(1e9)
+
+def dfs(graph, v, visited):
+    # graph, a에서 갈 수 있는 인접 노드들, 인접 행렬
+    visitied[v] = True
+    for node in graph[v]:
+        if not visited[node]:
+            dfs(graph, node, visited)
+
+def bfs(n):
+    # graph, a에서 갈 수 있는 인접 노드들, 인접 행렬
+    visited = [False] * (n+1)
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        now = queue.popleft()
+        for node in graph[now]:
+            if not visited[node]:
+                queue.append(node)
+                visited[node] = True
+
+def dijkstra(start):
+    # graph, a에서 갈 수 있는 인접 노드들, 인접 행렬
+    distance = [INF] * (n+1)
+
+    q = [(start, 0)]
+    while q:
+        now, dist = heapq.heappop()
+        if distance[now] < dist:
+            continue
+
+        for node in graph[now]:
+            cost = distance[now] + node[1]
+            if distance[node[0]] > cost:
+                distance[node[0]] = cost
+                heapq.heappush(q, (node[0], cost))
+
+
+def floyd():
+    # graph, a to b의 가중치, 2d arr
+    for k in range(1, n+1):
+        for a in range(1, n+1):
+            for b in range(1, n+1):
+                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+
+def bellman(start):
+    # edges, (a, b, val), 1d arr
+    for i in range(v):
+        for j in range(e):
+            a, b, c = edges[j]
+
+            if distance[b] > distance[a] + c:
+                distance[b] = distance[a] + c
+                if i == v - 1:
+                    return True
+    return False
