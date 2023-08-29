229859fb34d54a8d64a1576343dbfeed484e9bf9
Kwon770 <sckwon770@gmail.com>
2023-08-29 23:49:02 +0900

    goormthon: day11

---
+++ Goormthon/230814/day11.py
@@ -0,0 +1,21 @@
+import sys;
+
+readline = sys.stdin.readline
+
+N = int(readline())
+A, B = map(int, readline().split())
+
+ans = sys.maxsize
+for i in range(N // B + 1):
+    b = (B * i)
+    a = N - b
+    if a % A != 0:
+        continue
+
+    count = (a // A) + i
+    ans = min(ans, count)
+
+if ans == sys.maxsize:
+    print(-1)
+else:
+    print(ans)
