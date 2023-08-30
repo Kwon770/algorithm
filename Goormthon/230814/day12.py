25bd99ea7891704c71e39a10c801e6f74e0c312e
Kwon770 <sckwon770@gmail.com>
2023-08-31 00:33:43 +0900

    goormthon: day12

--- Goormthon/230814/day12.py
+++ Goormthon/230814/day12.py
@@ -1,12 +1,3 @@
-# https://www.acmicpc.net/problem/10775
-# 10775 공항
-# Memory: 35108 KB, Time: 144 ms, python3
-
-"""
-
-"""""
-
-
 import sys; readline = sys.stdin.readline
 # sys.setrecursionlimit(10 ** 6)
 
