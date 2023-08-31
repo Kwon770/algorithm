d8ff0a6129449f79618e87cc0e2c37ca5828a8c8
Kwon770 <sckwon770@gmail.com>
2023-09-01 08:51:49 +0900

    goormthon: day14

--- Goormthon/230814/day14.py
+++ Goormthon/230814/day14.py
@@ -1,11 +1,3 @@
-# https://www.acmicpc.net/problem/10775
-# 10775 공항
-# Memory: 35108 KB, Time: 144 ms, python3
-
-"""
-
-"""""
-
 import sys; readline = sys.stdin.readline
 
 N, M, K = map(int, readline().split())
